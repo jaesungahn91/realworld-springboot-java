624bc6115ad0e938e99cb76dd872b4a2b59b4aa5
ahnjs <ahnjs@eseict.com>
2021-11-01 21:57:49 +0900

    feat(security): security config add

---
+++ src/main/java/io/github/ahnjs/realworld/application/security/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package io.github.ahnjs.realworld.application.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableConfigurationProperties(SecurityConfigurationProperties.class)
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+    private final SecurityConfigurationProperties properties;
+
+    public SecurityConfiguration(SecurityConfigurationProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.formLogin().disable();
+        http.logout().disable();
+
+//        http.addFilterBefore();
+        http.authorizeRequests();
+    }
+
+
+}
+
+@ConstructorBinding
+@ConfigurationProperties("security")
+class SecurityConfigurationProperties {
+    private final List<String> allowedOrigins;
+
+    public SecurityConfigurationProperties(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+}
